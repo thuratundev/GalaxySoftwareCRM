f99a3757210133bf4357bb3e3be6943f7604be5d
Thura Tun <thura.kofat@gmail.com>
2022-08-25 14:12:33 +0630

    Add project files.

---
+++ GalaxySoftwareCRM/Server/Controllers/ApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace GalaxySoftwareCRM.Server.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public  IActionResult GetData(int param)
+        {
+            if(param == 0)
+            {
+                var json = new
+                {
+                    starttime = DateTimeOffset.Now,
+                    duration = TimeSpan.FromMilliseconds(10000)
+                };
+                return new JsonResult(value: json);
+            }
+            else
+            {
+                return RedirectToPage("/Error");
+            }
+            
+        }
+    }
+}
---
+++ GalaxySoftwareCRM/Shared/ApiHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxySoftwareCRM.Shared
+{
+    public class ApiHelper
+    {
+        public bool IsStoredProcedure { get; set; }
+        public string? SqlQuery { get; set; }
+        public string? StoredProcedureName { get; set; }
+        public object[]? Parameters { get; set; }
+        public SqlExecutionTypes SqlExecutionType { get; set; }
+    }
+
+    public enum SqlExecutionTypes
+    {
+        ExecuteOnly = 0,
+        ExecuteOutput,
+        ExecuteResult
+    }
+
+}
