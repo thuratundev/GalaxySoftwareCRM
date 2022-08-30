e28bf54367c2a34eddeaa65aa1ee667faf0b6728
Thura Tun <thura.kofat@gmail.com>
2022-08-30 17:37:50 +0630

    ActionService Added

---
+++ GalaxySoftwareCRM/Shared/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxySoftwareCRM.Shared
+{
+    public class Customer
+    {     
+        public int CustomerId { get; set; }
+        public string? Name { get; set; }
+        public string? ShortDesc { get; set; }
+        public string? Contact { get; set; }
+        public string? Address { get; set; }
+        public int? TownshipId { get; set; }
+    }
+}
